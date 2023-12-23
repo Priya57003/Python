157a3ecf58047a04c26f4bb3fe7a8c7e51af67fc
Priya57003 <96000441+Priya57003@users.noreply.github.com>
2023-12-23 06:21:29 -0800

    Create Conversion of numbers.py

---
+++ Conversion of numbers.py
@@ -0,0 +1,12 @@
+val1=0b1010
+print(val1) #binary to decimal
+val2=0o123
+print(val2) #Octal to decimal
+val3=0X123456789AD
+print(val3) #Hexadecimal to decimal
+
+print(bin(10)) #decimal to binary
+print(oct(10)) #decimal to Octal
+print(hex(10)) #decimal to Hexadecimal
+
+print("Octal to binary",bin(0o123))
